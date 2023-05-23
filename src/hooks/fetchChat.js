f83ba32f55df0b974e9a3f8d5bbb34e47dcf20ab
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-05-23 16:07:08 +0530

    use Fetch instead of axios

--- src/hooks/fetchChat.js
+++ src/hooks/fetchChat.js
@@ -17,18 +17,54 @@ function useFetchData(pageNumber){
     useEffect(()=>{
         setLoading(true);
         setError(false);
-        axios({
-            method: 'GET',
-            url: 'http://3.111.128.67/assignment/chat',
-            params: {page: 0}
-        }).then(res=>{
-            setSender(res.data.from);
-            setReceiver(res.data.to);
-            setTitle(res.data.name);
-            setHasMore(res.data.chats.length > 0);
+        // axios({
+        //     method: 'GET',
+        //     url: 'http://3.111.128.67/assignment/chat',
+        //     params: {page: 0}
+        // }).then(res=>{
+        //     setSender(res.data.from);
+        //     setReceiver(res.data.to);
+        //     setTitle(res.data.name);
+        //     setHasMore(res.data.chats.length > 0);
+
+        //     setImgs(prev=>{
+        //         let newImgs = [...new Set(res.data.chats.map((chat)=>{
+        //             return chat.sender.image;
+        //         }))];
+                
+        //         newImgs = newImgs.slice(0, 4);
+                
+        //         return newImgs;
+        //     })
+            
+            
+        //     // setChats(prev=> {
+        //     //     let newChats = res.data.chats.map(chat=>chat);
+        //     //     newChats = newChats.sort((a, b)=>{
+        //     //         return new Date(b.time).getTime() - new Date(a.time).getTime()
+        //     //     })
+        //     //     return [...prev, ...newChats];
+        //     // });
+                        
+        //     // console.log(res.data);
+        //     setLoading(false);
+            
+        // }).catch(e=>{
+        //     setError(true);
+        //     console.error(e);
+        // })
+
+
+        fetch('http://3.111.128.67/assignment/chat?page=0')
+        .then(res=>res.json())
+        .then(res=>{
+            setSender(res.from);
+            setReceiver(res.to);
+            setTitle(res.name);
+            setHasMore(res.chats.length > 0);
 
             setImgs(prev=>{
-                let newImgs = [...new Set(res.data.chats.map((chat)=>{
+                let newImgs = [...new Set(res.chats.map((chat)=>{
                     return chat.sender.image;
                 }))];
                 
@@ -36,15 +72,6 @@ function useFetchData(pageNumber){
                 
                 return newImgs;
             })
-            
-            
-            // setChats(prev=> {
-            //     let newChats = res.data.chats.map(chat=>chat);
-            //     newChats = newChats.sort((a, b)=>{
-            //         return new Date(b.time).getTime() - new Date(a.time).getTime()
-            //     })
-            //     return [...prev, ...newChats];
-            // });
                         
             // console.log(res.data);
             setLoading(false);
@@ -60,16 +87,37 @@ function useFetchData(pageNumber){
 
         setLoading(true);
         setError(false);
-        axios({
-            method: 'GET',
-            url: 'http://3.111.128.67/assignment/chat',
-            params: {page: pageNumber}
-        }).then(res=>{
+        // axios({
+        //     method: 'GET',
+        //     url: 'http://3.111.128.67/assignment/chat',
+        //     params: {page: pageNumber}
+        // }).then(res=>{
 
-            setHasMore(res.data.chats.length > 0);   
+        //     setHasMore(res.data.chats.length > 0);   
+            
+        //     setChats(prev=> {
+        //         let newChats = res.data.chats.map(chat=>chat);
+        //         newChats = newChats.sort((a, b)=>{
+        //             return new Date(b.time).getTime() - new Date(a.time).getTime()
+        //         })
+        //         return [...prev, ...newChats];
+        //     });
+                        
+        //     setLoading(false);
+            
+        // }).catch(e=>{
+        //     setError(true);
+        //     console.error(e);
+        // })
+
+        fetch(`http://3.111.128.67/assignment/chat?page=${pageNumber}`)
+        .then(res=>res.json())
+        .then(res=>{
+
+            setHasMore(res.chats.length > 0);   
             
             setChats(prev=> {
-                let newChats = res.data.chats.map(chat=>chat);
+                let newChats = res.chats.map(chat=>chat);
                 newChats = newChats.sort((a, b)=>{
                     return new Date(b.time).getTime() - new Date(a.time).getTime()
                 })
@@ -77,7 +125,7 @@ function useFetchData(pageNumber){
             });
                         
             setLoading(false);
-            
+
         }).catch(e=>{
             setError(true);
             console.error(e);
